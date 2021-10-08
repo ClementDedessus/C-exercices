4fa4f986fc3cc43e3f28c3775e02a94968a2f997
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-10-08 20:06:49 +0200

    add - prog2.c

---
+++ Semaine 4/prog2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Entrez le 2eme nombre : ");
+    scanf("%d", &a);
+    printf("Entrez le 2eme nombre : ");
+    scanf("%d", &b);
+    c = a * b;
+    printf("%d x %d = %d\n", a, b, c);
+    return 0;
+    printf("test \n");
+}
