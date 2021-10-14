05943e28a9f370f1d7a67ddec5d4fbd127cf2b41
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-10-14 14:21:29 +0200

    add - calcule de la moyenne

--- Semaine 5/prog1.c
+++ Semaine 5/prog1.c
@@ -1,9 +1,36 @@
-#include<stdlib.h>
+//gcc -std=c11 -pedantic -Werror -Wall -Wvla -Wno-unused-variable -o testProg1 prog1.c
+
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(){
+#define TAILLE 5
 
-    int valeur;
+int main()
+{
 
-    scanf("%d", &valeur);
+    int note;
+    int tabNote[TAILLE];
+    double total;
+    double moyenne;
+    int nombreNote;
+
+    printf("Veuillez entrer les notes de la classes \n");
+
+    for (int i = 0; i < TAILLE; i++)
+    {
+        printf("Veuillez entrer la note numero %d  : \n" , i );
+        scanf("%d", &note);
+        tabNote[i] = note;
+        nombreNote++;
+    }
+
+    for (int i = 0; i < TAILLE; i++)
+    {
+        total += tabNote[i];
+    }
+
+    moyenne = total / nombreNote;
+    return printf("%f",moyenne);
+    
+    exit(0);
 }
