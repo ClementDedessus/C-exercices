25ef17fcd411118f0b312f457f137903e377057c
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-11-08 11:57:24 +0100

    add - puissance4

--- Semaine 9/Prog4.2.c
+++ Semaine 9/Prog4.2.c
@@ -1,4 +1,38 @@
-//gcc -std=c11 -pedantic -Werror -Wall -Wvla -Wno-unused-variable -o testProg3.3.exe Prog3.3.c
+//gcc -std=c11 -pedantic -Werror -Wall -Wvla -Wno-unused-variable -o testProg4.2.exe Prog4.2.c
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int H = 0;
+    int L = 0;
+
+    printf("Veuillez donner la hauteur \n");
+    scanf("%c", &H);
+    printf("Veuillez donner la largeur \n");
+    scanf("%c", &L);
+
+    char **puissance;
+    puissance = (char **)malloc(H * sizeof(char *));
+    if (puissance == NULL)
+        exit(1);
+
+    for (int i = 0; i < H; i++)
+    {
+        puissance[i] = (char *)malloc(L * sizeof(char));
+        if (puissance[i] == NULL)
+            exit(1);
+    }
+
+
+
+
+
+
+
+
+    for (int i = 0; i < H; i++)
+        free(puissance[i]);
+    free(puissance);
+}
