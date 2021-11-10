90159fbee726a60336db9737f33cf51bcc4f01f5
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-11-10 10:11:02 +0100

    add - deplacementPiece et ReallocV1

--- Semaine 9/Prog4.2.c
+++ Semaine 9/Prog4.2.c
@@ -2,14 +2,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>  
-
+#include <stdbool.h>
 
 int main(int argc, char **argv)
 {
     int H = 0;
     int L = 0;
-    bool gagnant = false;
 
     printf("Veuillez donner la hauteur :\n");
     scanf("%d", &H);
@@ -52,13 +50,48 @@ int main(int argc, char **argv)
     int emplacementPiece;
     int hauteurPiece = H - 1;
     int tourDesJoeurs = 0;
-    while (gagnant == false) //condition  à modifier -> gagnant
+    int deplacementPiece = 0;
+    int i = 0;
+
+    while (tourDesJoeurs < 100) //condition  à modifier -> gagnant
     {
         if (tourDesJoeurs % 2 == 0)
         {
             printf("Dans quelle colonne voulez vous jouer (X) ? \n");
             scanf("%d", &emplacementPiece);
-            puissance[hauteurPiece][emplacementPiece - 1] = 'X';
+            //realoc
+            if (emplacementPiece > L)
+            {
+                printf("Augmentation de la taille du tableau \n");
+                puissance = (char **)realloc(puissance, emplacementPiece);
+                //on remplit le tableau
+                for (int i = 0; i < H; i++)
+                {
+                    for (int j = 0; j < L; j++)
+                    {
+                        puissance[i][j] = '.';
+                    }
+                }
+
+                //print le tableau
+                for (int i = 0; i < H; i++)
+                {
+                    for (int j = 0; j < L; j++)
+                    {
+                        printf("%4c", puissance[i][j]);
+                    }
+                    printf("\n");
+                }
+            }
+            //deplacement
+            while (puissance[hauteurPiece - i][emplacementPiece - 1] != '.')
+            {
+                i++;
+                deplacementPiece++;
+            }
+            puissance[hauteurPiece - deplacementPiece][emplacementPiece - 1] = 'X';
+            deplacementPiece = 0;
+            i = 0;
             for (int i = 0; i < H; i++)
             {
                 for (int j = 0; j < L; j++)
@@ -73,7 +106,14 @@ int main(int argc, char **argv)
         {
             printf("Dans quelle colonne voulez vous jouer (O) ? \n");
             scanf("%d", &emplacementPiece);
-            puissance[hauteurPiece][emplacementPiece - 1] = 'O';
+            while (puissance[hauteurPiece - i][emplacementPiece - 1] != '.')
+            {
+                i++;
+                deplacementPiece++;
+            }
+            puissance[hauteurPiece - deplacementPiece][emplacementPiece - 1] = 'O';
+            deplacementPiece = 0;
+            i = 0;
             for (int i = 0; i < H; i++)
             {
                 for (int j = 0; j < L; j++)
