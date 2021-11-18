714c39187f287357015126943ac051ff28753e63
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-11-18 17:16:00 +0100

    add - space

--- Semaine 10/prog5.1.2.c
+++ Semaine 10/prog5.1.2.c
@@ -9,12 +9,13 @@ int main()
     printf("Veuillez entrez un mot : \n");
     char ligne[TAILLE];
     while (fgets(ligne, TAILLE, stdin) != NULL)
-    {
+    {   
+        //renvoie le mot
         printf("Le mot lu est : %s", ligne);
 
         //renvoie la taille du mot
         printf("La taille du mot est de : %d", strlen(ligne) - 1);
-        
+
         //voir si le mot est bien dans la table
         /*if(){
             printf("present \n");
