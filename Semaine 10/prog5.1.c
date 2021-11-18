625d28fced22cc86c29aaad73721a644a41f9b06
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-11-18 16:16:35 +0100

    add - suite prog5.1

--- Semaine 10/prog5.1.c
+++ Semaine 10/prog5.1.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s \n", argv[i]);
+    }
 }
