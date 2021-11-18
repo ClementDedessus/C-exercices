9abfcb5636d36081432f2f05d4d3c4d96a28e737
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-11-18 17:08:36 +0100

    add - verifMotTable

--- Semaine 10/prog5.1.2.c
+++ Semaine 10/prog5.1.2.c
@@ -5,13 +5,20 @@
 #define TAILLE 256
 
 int main()
-{   
+{
     printf("Veuillez entrez un mot : \n");
     char ligne[TAILLE];
     while (fgets(ligne, TAILLE, stdin) != NULL)
     {
         printf("Le mot lu est : %s", ligne);
-        printf("La taille du mot est de : %d", strlen(ligne)-1);
+        //renvoie la taille du mot
+        printf("La taille du mot est de : %d", strlen(ligne) - 1);
+        //voir si le mot est bien dans la table
+        /*if(){
+            printf("present \n");
+        }else{
+            printf("absent \n");
+        }*/
         exit(0);
     }
 }
