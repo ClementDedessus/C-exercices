285d0d5fae717017928ce04f8cce5701cc16e2db
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-11-26 16:09:45 +0100

    add - fgets

--- Semaine 10/prog5.2.c
+++ Semaine 10/prog5.2.c
@@ -3,10 +3,22 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define TAILLE 256
+#define TAILLE 254
+#define ID_COFFRE 5
+#define NB_COFFRES 10
 
 int main(int argc, char *argv[])
 {
+    int taillePhys = NB_COFFRES;
+    int tailleLog = 0;
+    char **coffres = (char **)malloc(taillePhys * sizeof(char *));
+    if (coffres == NULL)
+    {
+        exit(1);
+    }
 
-
+    char ligne[TAILLE];
+    while (fgets(ligne, TAILLE, stdin) != NULL)
+    {
+    }
 }
