c644336a353322b0c9e631067fc2929c31ff18c0
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-02 11:36:16 +0100

    add - prog6.1.c

--- Semaine 12/prog6.1.c
+++ Semaine 12/prog6.1.c
@@ -1,7 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
+//RES: le maximum entre x et y
+int max(int x, int y)
+{
+    if (x > y)
+    {
+        return x;
+    }
+    if (x == y)
+    {
+        printf("Les deux sont egaux, donc le ");
+        return x;
+    }
+    return y;
+}
 
-    
+int main()
+{
+    int x;
+    x = max(10, 12);
+    printf("max = %d \n", x);
+    x = max(3, 10);
+    printf("max = %d \n", x);
+    x = max(9, 9);
+    printf("max = %d \n", x);
+    x = max(0, 9);
+    printf("max = %d \n", x);
+    x = max(0, 0);
+    printf("max = %d \n", x);
+    x = max(1, 20);
+    printf("max = %d \n", x);
 }
