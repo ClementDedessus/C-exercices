057f20b30b695744b8974f24f5c5298e4a03f661
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-02 12:06:45 +0100

    add - prog6.2.c

--- Semaine 12/prog6.2.c
+++ Semaine 12/prog6.2.c
@@ -1,6 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+//PRE : t : tableau de longueur sz
+//RES : true si le t contient un élément x, false sinon
+bool search(int *t, int sz, int x)
+{
+    t = malloc((sz) * sizeof(int));
+    if (!t)
+        exit(0);
+    for (int i = 0; i < sz; i++)
+    {
+        t[i] = i;
+    }
+    for (int i = 0; i < sz; i++)
+    {
+        if (t[i] == x)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main()
 {
+    int taille = 10;
+    int valeur = 15;
+    int *t = malloc(taille * sizeof(int));
+    bool test = search(t, taille, valeur);
+    printf("0 = false , 1 = true \n");
+    printf("%d", test);
 }
