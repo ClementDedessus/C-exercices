92b2e4bafc65255dddb618850c89d2606310ce3a
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-09 12:05:12 +0100

    add - prog6.4.c

---
+++ Semaine 12/prog6.4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//PRE : t : tableau de longueur sz 
+//POST : t représente un tableau trié contenant les sz premiers nombres premiers
+void first_prime_numbers(int *t, int sz)
+{
+    
+}
